56e3795dbcf609c5a0fea386f5b28a6429ad57a6
Michael Arce <lee.michael.anthony.arce@gmail.com>
2022-04-19 17:50:41 -0600

    formatted color widget

--- src/widgets/Color.js
+++ src/widgets/Color.js
@@ -22,27 +22,26 @@ export default function Colors() {
         setColors(newColor)
     }
     function hexify(num){
-        return parseInt(num, 10).toString(16).toUpperCase()
-    }
-return(
-    <div>
-        <h1 className="Styler" style={{color: colors}}>Style me</h1>
-        <div className="sliders">
-            <div>
-                <input type="range" max="255" value={red} className="slider" id="Red" onInput={e => changeRed(e.target.value)}></input>
-                <h6>Red: {red}</h6>
-            </div>
-            <div>
-                <input type="range"  max="255" value={green} className="slider" id="Green" onInput={e => changeGreen(e.target.value)}></input>
-                <h6>Green: {green}</h6>
-            </div>
-            <div>
-                <input type="range"  max="255" value={blue} className="slider" id="Blue" onInput={e => changeBlue(e.target.value)}></input>
-                <h6>Blue: {blue}</h6>
-            </div>
-        </div>
-        <h2>#{hexify(red)+hexify(green)+hexify(blue)}</h2>
-    </div>
-)
+        return parseInt(num, 10).toString(16).toUpperCase().padStart(2,0)
+        }
 
-}
+    return(
+        <div>
+            <h1 className="Styler" style={{color: colors}}>Style me</h1>
+            <div className="sliders">
+                <div>
+                    <input type="range" max="255" value={red} className="slider" id="Red" onInput={e => changeRed(e.target.value)}></input>
+                    <h6>Red: {red}</h6>
+                </div>
+                <div>
+                    <input type="range"  max="255" value={green} className="slider" id="Green" onInput={e => changeGreen(e.target.value)}></input>
+                    <h6>Green: {green}</h6>
+                </div>
+                <div>
+                    <input type="range"  max="255" value={blue} className="slider" id="Blue" onInput={e => changeBlue(e.target.value)}></input>
+                    <h6>Blue: {blue}</h6>
+                </div>
+            </div>
+            <h2>#{hexify(red)+hexify(green)+hexify(blue)}</h2>
+        </div>
+    )}
